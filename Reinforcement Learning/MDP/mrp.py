434f71db1d0b761bafd71ea796af1d621ec386cd
Soham Phanse <sohamphansebramhos@gmail.com>
2026-03-18 00:34:52 -0700

    enforce object oriented architecture

---
+++ Reinforcement Learning/MDP/mrp.py
@@ -0,0 +1,71 @@
+"""
+This file will implement the Markov Reward Process (MDP) class.
+"""
+
+import numpy as np
+from typing import Callable, Tuple
+
+class MRP():
+
+    """
+    ns: number of states in the MRP
+    allS: all states
+    S: state transition function
+    Sd: dictionary of states ['state name': state index]
+    P: state transition matrix
+    R: reward function (expected E[R_{t+1} | S_t = s])
+    Rd: dictionary of rewards
+    gamma: discount factor
+    S0: initial state
+    """
+    def __init__(self, 
+                 ns: int,
+                 allS: list[int],
+                 S: Callable[[np.ndarray, int, list[int]], int],
+                 Sd: dict[str, int], 
+                 P: np.ndarray, 
+                 R: Callable[[int, np.ndarray, dict], float], 
+                 Rd: dict[str, int],
+                 gamma: float,
+                 S0: int):
+
+        self.ns, self.allS, self.S, self.Sd, \
+        self.P, self.R, self.Rd, \
+        self.gamma, self.S0 = ns, allS, S, Sd, P, R, Rd, gamma, S0
+
+        self.SInvD = {v: k for k, v in self.Sd.items()}
+        self.RInvD = {v: k for k, v in self.Rd.items()}
+     
+    """
+    Inputs:
+    1. ts: Number of time steps to simulate, an integer.
+    """
+    def simulateMRP(self, ts: int) -> Tuple[np.ndarray, np.ndarray]:
+
+        # create arrays to store consecutive states
+        states = np.zeros(ts, dtype=int)
+        rewards = np.zeros(ts-1, dtype=float)
+
+        states[0] = self.S0
+
+        for i in range(1, ts):
+
+            scurr = states[i-1] # current state
+            states[i] = self.S(self.P, scurr, self.allS) # next state
+            rewards[i-1] = self.Rd[self.SInvD[states[i]]]
+
+        return states, rewards
+    
+    def computeReturn(self):
+
+        states, rewards = self.simulateMRP(self.P, self.Rd, self.SInvD, self.ts)
+        returns = np.zeros_like(rewards)
+        returns[-1] = rewards[-1]
+        for t in range(len(rewards) - 2, -1, -1):
+            returns[t] = rewards[t] + self.gamma * returns[t+1]
+
+        return returns
+
+
+
+        
---
+++ Reinforcement Learning/MDP/utils.py
@@ -0,0 +1,42 @@
+"""
+This file will contain the   state transition matrix, the reward model, and the states
+"""
+
+import numpy as np
+"""
+Inputs:
+1. P: Transition probability matrix of size (n, n) where n is the number of states.
+2. s: Current state (integer index).
+3. all_s: List of all possible states.
+"""
+def stateTransition(P: np.ndarray, 
+                    s: int, 
+                    all_s: list[int]) -> int:
+
+    # get transition probabilities for this state
+    tP = P[s]
+
+    # sample the next state, with tP probabilities
+    next_s = np.random.choice(all_s, p=tP)
+
+    return next_s
+
+"""
+Inputs:
+1. s: current state (integer index).
+2. P: Transition probability matrix of size (n, n) where n is the number of states.
+3. R: Reward function, a dictionary mapping state index to reward value.
+"""
+
+def rewardFunction(s: int, 
+                   P: np.ndarray, 
+                   R: dict)-> float:
+
+    # find all possible next states, and their probabilities
+    nextStateProbs = P[s]
+
+    # do a weighted average with all states, and their rewards
+    reward = np.average(list(R.values()), weights=nextStateProbs)
+
+    return reward
+
